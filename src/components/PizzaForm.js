b03e150464af808dba38e2ac7203afe69e937d72
alexander <alexandernyaga1999@gmail.com>
2023-12-31 11:57:25 +0300

    Flatiron Pizza

--- src/components/PizzaForm.js
+++ src/components/PizzaForm.js
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 function PizzaForm() {
+  // store form data
+  const [formData, setFormData] = useState({
+    topping: "",
+    size: "Small",
+    vegetarian: "Vegetarian",
+  });
+
+  // handle form input changes
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
+  //Handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form submitted:", formData);
+    // Reset form data 
+    setFormData({
+      topping: "",
+      size: "Small",
+      vegetarian: "Vegetarian",
+    });
+  };
+
   return (
-    <form onSubmit={null /*handle that submit*/}>
+    <form onSubmit={handleSubmit}>
       <div className="form-row">
         <div className="col-5">
           <input
@@ -10,10 +38,17 @@ function PizzaForm() {
             type="text"
             name="topping"
             placeholder="Pizza Topping"
+            value={formData.topping}
+            onChange={handleInputChange}
           />
         </div>
         <div className="col">
-          <select className="form-control" name="size">
+          <select
+            className="form-control"
+            name="size"
+            value={formData.size}
+            onChange={handleInputChange}
+          >
             <option value="Small">Small</option>
             <option value="Medium">Medium</option>
             <option value="Large">Large</option>
@@ -26,6 +61,8 @@ function PizzaForm() {
               type="radio"
               name="vegetarian"
               value="Vegetarian"
+              checked={formData.vegetarian === "Vegetarian"}
+              onChange={handleInputChange}
             />
             <label className="form-check-label">Vegetarian</label>
           </div>
@@ -35,6 +72,8 @@ function PizzaForm() {
               type="radio"
               name="vegetarian"
               value="Not Vegetarian"
+              checked={formData.vegetarian === "Not Vegetarian"}
+              onChange={handleInputChange}
             />
             <label className="form-check-label">Not Vegetarian</label>
           </div>
--- src/components/PizzaList.js
+++ src/components/PizzaList.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Pizza from "./Pizza";
 
-function PizzaList() {
+function PizzaList({ Pizzas }) {
+  const [pizzas, setPizzas] = useState([]);
+
+  useEffect(() => {
+    // Fetch pizza data from the json-server
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:3002/pizzas"); // Replace with your json-server URL
+        const data = await response.json();
+        setPizzas(data);
+      } catch (error) {
+        console.error("Error fetching pizza data:", error);
+      }
+    };
+
+    fetchData();
+  }, []); // Empty dependency array ensures that the effect runs only once on component mount
+
   return (
     <table className="table table-striped">
       <thead>
@@ -13,9 +30,15 @@ function PizzaList() {
         </tr>
       </thead>
       <tbody>
-        {
-          //render Pizza here
-        }
+        {pizzas.map((pizza) => (
+          <Pizza
+            key={pizza.id} // Assuming each pizza has a unique identifier
+            topping={pizza.topping}
+            size={pizza.size}
+            vegetarian={pizza.vegetarian}
+            // Add other pizza properties as needed
+          />
+        ))}
       </tbody>
     </table>
   );
